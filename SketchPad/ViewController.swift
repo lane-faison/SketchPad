23d123d7bae6ac532b9209e3e07ba5520274d1f6
Lane Faison <lane.faison@icloud.com>
2018-07-18 23:17:51 -0600

    Initial commit

--- SketchPad/ViewController.swift
+++ SketchPad/ViewController.swift
@@ -1,25 +1,11 @@
-//
-//  ViewController.swift
-//  SketchPad
-//
-//  Created by Lane Faison on 7/18/18.
-//  Copyright © 2018 Lane Faison. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
